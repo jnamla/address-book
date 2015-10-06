5ec202510c4128c3047e8e0b8db4624543649e05
Johanna <jnamla@gmail.com>
2015-10-06 14:39:53 +0000

    delete folder node-v4.1.0-linux-x64

--- bin/config.js
+++ bin/config.js
@@ -3,8 +3,7 @@ var config = {};
 //config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/cymplar';
 
 config.mongodb = {
-  //uri: process.env.MONGOLAB_URI ||  "mongodb://root:password@ds043981.mongolab.com:43981/cymplar"
-  uri: process.env.MONGOLAB_URI ||  'mongodb://localhost:27017/cymplar'
+  uri: process.env.MONGOLAB_URI ||  "mongodb://root:password@ds043981.mongolab.com:43981/cymplar"
 };
 
 config.secretKey = "cymplarSecret";
